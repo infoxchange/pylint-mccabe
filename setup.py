495a117cd122ce0d22b7c603eaf2bbdef3df0df9
Alexey Kotlyarov <alexey@infoxchange.net.au>
2013-07-12 08:41:20 +1000

    Bump the version, update dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pylint-mccabe',
-    version='0.1.2',
+    version='0.1.3',
     author='Infoxchange Australia dev team',
     author_email='devs@infoxchange.net.au',
     py_modules=['pylint_mccabe'],
@@ -15,7 +15,7 @@ setup(
     long_description=open('README.rst').read(),
     install_requires=[
         'mccabe >= 0.2',
-        'pep8 >= 1.4.5',
+        'pep8 >= 1.4.6',
         'pylint >= 0.28.0',
     ],
 )
